6d5e1f4119c38caf1b91718ca896e1d93686fdff
Pussycat4 <135804685+Pussycat4@users.noreply.github.com>
2023-06-07 14:19:03 +0200

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -10,15 +10,26 @@ def install_package(package):
 def download_file(url, filename):
     urllib.request.urlretrieve(url, filename)
 
-def install_missing_packages(required_packages):
+def install_missing_packages(required_packages, max_attempts=3):
     installed_packages = [pkg.key for pkg in pkg_resources.working_set]
     missing_packages = [pkg for pkg in required_packages if pkg not in installed_packages]
 
     if missing_packages:
-        print("Folgende Pakete werden installiert: ")
+        print("Folgende Pakete werden installiert:")
         for package in missing_packages:
             print(package)
-            install_package(package)
+        
+        for attempt in range(max_attempts):
+            try:
+                for package in missing_packages:
+                    install_package(package)
+                print("Die fehlenden Pakete wurden erfolgreich installiert.")
+                return
+            except subprocess.CalledProcessError:
+                print(f"Die Installation der fehlenden Pakete ist fehlgeschlagen. Versuch {attempt+1} von {max_attempts}.")
+        
+        print("Die Installation der fehlenden Pakete ist fehlgeschlagen. Bitte überprüfen Sie Ihre Internetverbindung und versuchen Sie es erneut.")
+        return
 
 # Erforderliche Pakete
 required_packages = [
@@ -32,7 +43,7 @@ required_packages = [
 ]
 
 # Installieren der fehlenden Pakete
-install_missing_packages(required_packages)
+install_missing_packages(required_packages, max_attempts=3)
 
 # Aktuelles Arbeitsverzeichnis
 current_directory = os.path.dirname(os.path.abspath(__file__))
