5870cc7c001c8cf5f1c8d0ec2fda62df42054691
Pussycat4 <135804685+Pussycat4@users.noreply.github.com>
2023-06-07 13:50:52 +0200

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -1,46 +1,44 @@
 import os
-
 import sys
-
 import subprocess
+import urllib.request
 
 def install_package(package):
-
     subprocess.check_call([sys.executable, "-m", "pip", "install", package, "--target", "."])
 
 # Installation von tqdm
-
 install_package("tqdm")
 
 # Liste der Bibliotheken
-
 libraries = [
-
     "SpeechRecognition",
-
     "PyAudio",
-
     "pyttsx3",
-
     "nltk",
-
     "torch" if torch_available else "tensorflow",
-
     "opencv-python",
-
     "pydub"
-
 ]
 
 # Aktuelles Arbeitsverzeichnis
-
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Installation der Bibliotheken im aktuellen Verzeichnis
-
 for library in tqdm(libraries, desc="Herunterladen und Installieren"):
-
     install_package(library)
 
 print("Die Bibliotheken wurden erfolgreich heruntergeladen und installiert.")
 
+# URL des Codes
+code_url = "https://raw.githubusercontent.com/Pussycat4/Sprachassistent/main/Start.py"
+
+# Zielpfad zum Speichern des heruntergeladenen Codes
+code_file = os.path.join(current_directory, "Start.py")
+
+# Herunterladen des Codes
+print("Lade den Code herunter...")
+urllib.request.urlretrieve(code_url, code_file)
+
+# Ausführen des heruntergeladenen Codes
+print("Führe den heruntergeladenen Code aus...")
+exec(open(code_file).read())
