2788165446e4c8ab62e84a36a54d4e7780913551
Pussycat4 <135804685+Pussycat4@users.noreply.github.com>
2023-06-07 17:35:25 +0200

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -2,18 +2,20 @@ import os
 import urllib.request
 
 def download_file(url, filename):
-    urllib.request.urlretrieve(url, filename)
+    try:
+        urllib.request.urlretrieve(url, filename)
+        print(f"Die Datei '{filename}' wurde erfolgreich heruntergeladen.")
+    except Exception as e:
+        print(f"Fehler beim Herunterladen der Datei: {e}")
 
 # URL der Datei zum Herunterladen
 url = "https://raw.githubusercontent.com/Pussycat4/Sprachassistent/main/Start.py"
 
 # Pfad zur aktuellen Verzeichnis
-current_directory = os.getcwd()
+current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Dateiname für die heruntergeladene Datei
 filename = os.path.join(current_directory, "Start.py")
 
 # Herunterladen der Datei
 download_file(url, filename)
-
-print("Die Datei 'Start.py' wurde erfolgreich heruntergeladen.")
