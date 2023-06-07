381a7692f26828eed6d040e2bd14b9aba092b63f
Pussycat4 <135804685+Pussycat4@users.noreply.github.com>
2023-06-07 14:15:46 +0200

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -2,17 +2,26 @@ import os
 import subprocess
 import urllib.request
 
+import pkg_resources
+
 def install_package(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package, "--target", "."])
 
 def download_file(url, filename):
     urllib.request.urlretrieve(url, filename)
 
-# Installation von tqdm
-install_package("tqdm")
+def install_missing_packages(required_packages):
+    installed_packages = [pkg.key for pkg in pkg_resources.working_set]
+    missing_packages = [pkg for pkg in required_packages if pkg not in installed_packages]
 
-# Liste der Bibliotheken
-libraries = [
+    if missing_packages:
+        print("Folgende Pakete werden installiert: ")
+        for package in missing_packages:
+            print(package)
+            install_package(package)
+
+# Erforderliche Pakete
+required_packages = [
     "SpeechRecognition",
     "PyAudio",
     "pyttsx3",
@@ -22,14 +31,11 @@ libraries = [
     "pydub"
 ]
 
+# Installieren der fehlenden Pakete
+install_missing_packages(required_packages)
+
 # Aktuelles Arbeitsverzeichnis
 current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Installation der Bibliotheken im aktuellen Verzeichnis
-for library in tqdm(libraries, desc="Herunterladen und Installieren"):
-    install_package(library)
-
-print("Die Bibliotheken wurden erfolgreich heruntergeladen und installiert.")
 
 # Download des Codes von der URL
 download_file("https://raw.githubusercontent.com/Pussycat4/Sprachassistent/main/Start.py", "Start.py")
